e683b04f1af604ab47f9fdd66b29f5a68de917b4
pezzonovante09 <denizait2406@gmail.com>
2025-01-31 19:36:20 +0500

    likes

--- apps/like/urls.py
+++ apps/like/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import LikeViewSet
 
 urlpatterns = [
-    path('posts/<int:post_id>/like/', LikeViewSet.as_view({'post': 'create'}), name='post-like'),
+    path('like/<int:post_id>/', LikeViewSet.as_view({'post': 'create'}), name='post-like'),
 ]
