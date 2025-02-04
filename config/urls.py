92a47b5616027eec6e11d64be05801a3b9bc29e4
Khann777 <nurdinov.d777@gmail.com>
2025-02-04 18:35:23 +0600

    configured swagger

--- config/urls.py
+++ config/urls.py
@@ -1,35 +1,33 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf.urls.static import static
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
 from rest_framework import permissions
 
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from . import settings
+
 
 schema_view = get_schema_view(
-    openapi.Info(
-        title="API документация",
-        default_version='v1',
-        description="Документация API для вашего проекта",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="support@example.com"),
-        license=openapi.License(name="MIT License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
+   openapi.Info(
+      title="shop_api",
+      default_version='v1',
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   # permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Swagger UI (графическая документация)
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-
-    # ReDoc (альтернативный UI для документации)
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
-    # JSON Schema
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 
     path('api/account/', include('apps.account.urls')),
     path('api/post/', include('apps.post.urls')),
